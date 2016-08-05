b72d961f547a7cd2e8fe2b4980a8b9f3269300b7
Surasak <jojo141a@hotmailcom>
2016-08-05 14:19:22 +0700

    add main fragment, java template files

--- app/src/main/java/com/nongmah/liveat500px/MainApplication.java
+++ app/src/main/java/com/nongmah/liveat500px/MainApplication.java
@@ -2,6 +2,8 @@ package com.nongmah.liveat500px;
 
 import android.app.Application;
 
+import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+
 /**
  * Created by jojo1 on 03-Aug-16.
  */
@@ -12,7 +14,7 @@ public class MainApplication extends Application {
         super.onCreate();
 
         // initialize thing(s) here
-
+        Contextor.getInstance().init(getApplicationContext());
     }
 
     @Override
