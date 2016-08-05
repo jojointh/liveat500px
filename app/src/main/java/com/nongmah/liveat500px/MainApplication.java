b181cc3c16ca00f1fb3c84f80b48a8ae60caffce
Surasak <jojo141a@hotmailcom>
2016-08-05 13:48:44 +0700

    add java class files

---
+++ app/src/main/java/com/nongmah/liveat500px/MainApplication.java
@@ -0,0 +1,23 @@
+package com.nongmah.liveat500px;
+
+import android.app.Application;
+
+/**
+ * Created by jojo1 on 03-Aug-16.
+ */
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // initialize thing(s) here
+
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+
+    }
+}
---
+++ app/src/main/java/com/nongmah/liveat500px/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nongmah.liveat500px.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nongmah.liveat500px.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
