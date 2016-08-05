e6dc8b5265cffdbc6010d26574841f2daf1ed9bf
Surasak <jojo141a@hotmailcom>
2016-08-05 21:45:28 +0700

    example of recycle view with multiple view type

--- app/src/main/java/com/nongmah/liveat500px/adapter/PhotoListAdapter.java
+++ app/src/main/java/com/nongmah/liveat500px/adapter/PhotoListAdapter.java
@@ -3,6 +3,7 @@ package com.nongmah.liveat500px.adapter;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import com.nongmah.liveat500px.view.PhotoListItem;
 
@@ -27,13 +28,35 @@ public class PhotoListAdapter extends BaseAdapter {
     }
 
     @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position % 2 == 0 ? 0 : 1;
+    }
+
+    @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        PhotoListItem item;
-        if (view != null)
-            item = (PhotoListItem) view;
-        else {
-            item = new PhotoListItem(viewGroup.getContext());
+        if (getItemViewType(i) == 0) {
+            PhotoListItem item;
+            if (view != null)
+                item = (PhotoListItem) view;
+            else {
+                item = new PhotoListItem(viewGroup.getContext());
+            }
+            return item;
+        } else {
+            TextView item;
+            if (view != null)
+                item = (TextView) view;
+            else {
+                item = new TextView(viewGroup.getContext());
+            }
+            item.setText("Position: " + i);
+            return item;
         }
-        return item;
+
     }
 }
