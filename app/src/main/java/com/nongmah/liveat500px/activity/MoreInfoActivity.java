b69c0eb3c1f381bc6cf51b89f8bc2952d491866b
JoJo <jojo141a@hotmail.com>
2016-08-08 03:51:52 +0700

    add up button to back to main activity

--- app/src/main/java/com/nongmah/liveat500px/activity/MoreInfoActivity.java
+++ app/src/main/java/com/nongmah/liveat500px/activity/MoreInfoActivity.java
@@ -2,6 +2,7 @@ package com.nongmah.liveat500px.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import com.nongmah.liveat500px.R;
@@ -15,11 +16,27 @@ public class MoreInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more_info);
+        
+        initInstances();
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.contentContainer, MoreInfoFragment.newInstance())
                     .commit();
         }
+    }
+
+    private void initInstances() {
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return  true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
